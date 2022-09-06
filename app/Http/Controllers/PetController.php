1393d55e3903652362247b38701af154a1313d53
zaqy-ramadhan <110901278+zaqy-ramadhan@users.noreply.github.com>
2022-09-06 12:23:16 +0700

    Add files via upload

---
+++ app/Http/Controllers/PetController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pet;
+use App\Http\Requests\StorePetRequest;
+use App\Http\Requests\UpdatePetRequest;
+
+class PetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePetRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePetRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pet  $pet
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pet $pet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pet  $pet
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pet $pet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePetRequest  $request
+     * @param  \App\Models\Pet  $pet
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePetRequest $request, Pet $pet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pet  $pet
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pet $pet)
+    {
+        //
+    }
+}
