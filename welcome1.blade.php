143c2d5ead95f1099a293e53cf64fe06ca9f15e4
pramadanif <100917342+pramadanif@users.noreply.github.com>
2022-09-24 18:02:39 +0700

    Add files via upload

---
+++ welcome1.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.1/css/bootstrap.min.css" integrity="sha512-siwe/oXMhSjGCwLn+scraPOWrJxHlUgMBMZXdPe2Tnk3I0x3ESCoLz7WZ5NTH6SZrywMY+PB1cjyqJ5jAluCOg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+  
+<div class="container d-flex justify-content-center">
+<form>
+  <div class="row">
+  <form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Nama Depan</label>
+    <input type="email" class="form-control" id="inputEmail4">
+    <div id="emailHelp" class="form-text">Masukkan Nama Depan.</div>
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Nama Belakang</label>
+    <input type="password" class="form-control" id="inputPassword4">
+    <div id="emailHelp" class="form-text">Masukkan Nama Belakang.</div>
+
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Alamat</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">Masukkan Nama Jalan.</div>
+  </div>
+  <div class="mb-3">
+  <label for="exampleInputEmail1" class="form-label">Nama Kabupaten/Kota</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">Masukkan Nama Kabupaten/Kota.</div>
+  </div>
+
+</div>
+  
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Tanggal Janjian</label>
+    <input type="date" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3">
+  <select class="form-select" aria-label="Default select example">
+  <option selected>Pilih Sesi Kunjungan</option>
+  <option value="1">08:00-12:00 (Sesi 1)</option>
+  <option value="2">13:00-17:00 (Sesi 2)</option>
+  <option value="3">18:00-22:00 (Sesi 3)</option>
+</select>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Nomor telepon</label>
+    <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">Masukkan Nomor Telepon.</div>
+  </div>
+  <div class="mb-3">
+  <label for="exampleInputEmail1" class="form-label">Jenis Hewan</label>
+  <select class="form-select" aria-label="Default select example">
+  <option selected>Pilih Jenis Hewan</option>
+  <option value="1">Anjing</option>
+  <option value="2">Kucing</option>
+  <option value="3">Serigala</option>
+  <option value="4">Harimau</option>
+  <option value="5">Rusa</option>
+  <option value="6">Burung</option>
+</select>
+  </div>
+  <div class="mb-3">
+  <label for="exampleInputEmail1" class="form-label">Jenis Hewan</label>
+  <select class="form-select" aria-label="Default select example">
+  <option selected>Pilih Jenis Kelamin</option>
+  <option value="1">Jantan</option>
+  <option value="2">Betina</option>
+  </select>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Tanggal Lahir Hewan</label>
+    <input type="date" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Berat Hewan</label>
+    <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">Masukkan Berat Hewan (KG).</div>
+  </div>
+  <div class="mb-3">
+  <label for="exampleInputEmail1" class="form-label">Keluhan Saat ini</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">Masukkan Keluhan/Penyakit.</div>
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.1/js/bootstrap.min.js" integrity="sha512-vyRAVI0IEm6LI/fVSv/Wq/d0KUfrg3hJq2Qz5FlfER69sf3ZHlOrsLriNm49FxnpUGmhx+TaJKwJ+ByTLKT+Yg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
